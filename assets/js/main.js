5d467bb455a79bb718f5c22baef35bda5cb544a9
khaisendefo <khaisendefo@gmail.com>
2024-09-27 15:05:13 +0300

    Created and mobile version header

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const toggleMenu = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.header__burger');
+    const nav = document.querySelector('.header__nav');
+    const navLinks = document.querySelectorAll('.header__nav-link');
+    const body = document.body;
+
+    function closeNav() {
+      toggleButton.classList.remove('active');
+      nav.classList.remove('active');
+      body.classList.remove('no-scroll');
+    }
+
+    toggleButton.addEventListener('click', function () {
+      const isActive = toggleButton.classList.toggle('active');
+      nav.classList.toggle('active');
+      if (isActive) {
+        body.classList.add('no-scroll');
+      } else {
+        body.classList.remove('no-scroll');
+      }
+    });
+
+    document.addEventListener('click', function(event) {
+      if (!nav.contains(event.target) && !toggleButton.contains(event.target)) {
+        closeNav();
+      }
+    });
+
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Escape' && nav.classList.contains('active')) {
+        closeNav();
+      }
+    });
+
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener('click', function() {
+        closeNav();
+      });
+    });
+  });
+}
+toggleMenu();
